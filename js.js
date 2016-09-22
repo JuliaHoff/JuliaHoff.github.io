6c54eca4b9c49298ca20aaff6ca85c79823c8a5b
JuliaHoff <julia.hoff@gmail.com>
2016-09-22 22:29:55 +0200

    Update and rename maps.js to js.js

---
+++ js.js
@@ -0,0 +1,16 @@
+
+var counter =0;
+window.setInterval(
+function () {
+counter = counter + 1;
+document.getElementById("counter").innerHTML = counter;
+}, 150);
+
+// 26042 per sek
+
+var counter2 =0;
+window.setInterval(
+function () {
+counter2 = counter2 + 1;
+document.getElementById("counter2").innerHTML = counter2;
+}, 150);
